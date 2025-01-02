16fa29116868dfb29e3c459f425bc7dbbf31ec38
jw-wcv <101585096+jw-wcv@users.noreply.github.com>
2025-01-02 11:33:24 -0800

    Update main.tf

--- cluster-manager/terraform/main.tf
+++ cluster-manager/terraform/main.tf
@@ -4,9 +4,9 @@ resource "aleph_compute_instance" "worker" {
   count = 3
   name  = "DistributedComputeWorker"
   image = "aleph/node"
-  cpu   = 2
-  memory = 4096
-  storage = 50
+  cpu   = 4
+  memory = 8192
+  storage = 80
 
   authorized_keys = ["${var.ssh_public_key}"]
 
